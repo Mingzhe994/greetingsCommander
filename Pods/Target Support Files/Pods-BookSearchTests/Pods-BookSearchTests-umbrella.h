cdb7f5c43f07726765d71e1516aa9bd85b611de1
Stuart <tuzuki@126.com>
2016-05-23 09:14:46 +1000

    Change Cocoa pod to use framework : )

---
+++ Pods/Target Support Files/Pods-BookSearchTests/Pods-BookSearchTests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_BookSearchTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_BookSearchTestsVersionString[];
+
---
+++ Pods/Target Support Files/Pods-BookSearchUITests/Pods-BookSearchUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BookSearchUITests : NSObject
+@end
+@implementation PodsDummy_Pods_BookSearchUITests
+@end
